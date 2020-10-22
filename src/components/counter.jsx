0bd3181686d745a0712339d273a6be230b03c369
thealekid <chukorakwusi@gmail.com>
2020-10-22 17:18:50 +0100

    CounterApp

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,21 +3,32 @@ import React, { Component } from 'react'
 class Counter extends Component {
 
     state = {
-        count: 0
+        count: 0,
     };
 
-    styles = {
-        fontSize: "50px",
-        fontWeight: "bold"
+    handleIncrement = (product) => {
+        console.log(product)
+        this.setState({count: this.state.count + 1});
+    };
+
+    doHandleIncrement = () => {
+        this.handleIncrement({id:1});
     };
 
     render() { 
+
         return (
         <div>
-            <span style={ {fontSize: 30 }} className="badge badge-primary m-2">{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button onClick={this.doHandleIncrement} className="btn btn-secondary btn-sm">Increment</button>
         </div>
         )
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCount () {
